468fd75df9bc3c5d8a46a249da232d9d61fde3ab
JamesFerguson <james.ferguson7@gmail.com>
2011-08-22 13:22:10 +1000

    Better printing?

--- lib/taskpaper/file.rb
+++ lib/taskpaper/file.rb
@@ -3,30 +3,31 @@ module TaskPaper
     attr_accessor :lists_hash
     
     def initialize(lists_matrix)
-      @lists_hash = parse_matrix(lists_matrix)
+      @lists_matrix = lists_matrix
     end
     
     def to_s
-      @lists_hash[:projects].reduce("Projects:\n") do |str, project|
-        project_name, (project_link, (project_next_step, *)) = project
+      last_project = []
+      
+      @lists_matrix.reduce("Projects:\n") do |str, project|
+        tabs = ""
+        project_name, project_link, (project_next_step, *) = project
+        project_name = project_name.split(/:/)
+        project_name.each.with_index do |name_part, index|
+          if index < project_name.size - 1 && last_project[index] != name_part
+            str += "#{tabs}	#{name_part}:\n"
+          end
+          tabs += "\t"
+        end
         
         str += <<-PROJECT
-	#{project_name} (#{project_link}):
-		- #{project_next_step}
+#{tabs}#{project_name.last} (#{project_link}):
+#{tabs}	- #{project_next_step}
 PROJECT
+        
+        last_project = project_name
+        str
       end
-    end
-    
-    private
-    
-    def parse_matrix(lists_matrix)
-      lists_hash = {:projects => {}}
-      
-      lists_matrix.each do |list_arr|
-        lists_hash[:projects][list_arr.first] = list_arr[1..list_arr.size]
-      end
-      
-      lists_hash
     end
   end
 end
